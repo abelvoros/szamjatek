b3b9fc37145b6c70672a9ddcbe7a8456b3cd3928
Abel Voros <vorosabel25@gmail.com>
2021-03-09 21:12:10 +0200

    Cleaner version

--- jatek.py
+++ jatek.py
@@ -4,7 +4,7 @@ print("Szamjatek")
 name=input("Neved?\n")
 print("Szervusz {}!".format(name))
 print("Jatekszabaly: Gondolok egy szamra 1-tol 100-ig, talald ki!")
-x=input("Van-e kedved jatszani? [y/n]:")
+x=input("Van-e kedved jatszani? [y/n]: ")
 if x!="y":
   print("Viszlat {}!".format(name))
   exit(0)
@@ -15,11 +15,11 @@ random.seed(1)
 number=random.randint(1, 100)
 tip=0
 while tip!=number:
-  tip_str=input("Tippelj!")
+  tip_str=input("Tippelj! ")
   tip=int(tip_str)
   if tip>number:
     print("Kisebb...")
   elif tip<number:
     print("Nagyobb...")
-print("Eltalaltad {}, a szam valoban {} volt".format(name,number))
+print("Eltalaltad {}, a szam valoban {} volt!".format(name,number))
 
