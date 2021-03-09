472e718dd53594329704d558f1dde5695f86e0b3
Abel Voros <vorosabel25@gmail.com>
2021-03-09 19:45:56 +0200

    Added questioning part

--- jatek.py
+++ jatek.py
@@ -1,3 +1,5 @@
+import random
+
 print("Szamjatek")
 name=input("Neved?\n")
 print("Szervusz {}!".format(name))
@@ -8,3 +10,16 @@ if x!="y":
   exit(0)
 
 print("Jatek kezdodik")
+
+random.seed(1)
+number=random.randint(1, 100)
+tip=0
+while tip!=number:
+  tip_str=input("Tippelj!")
+  tip=int(tip_str)
+  if tip>number:
+    print("Kisebb...")
+  elif tip<number:
+    print("Nagyobb...")
+print("Eltalaltad {}, a szam valoban {} volt".format(name,number))
+
